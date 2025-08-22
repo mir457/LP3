5ed9e259134a6ae09bf7ffe24311fb1eb99fac28
mir457 <miranda.benavente@estudiante.ucsm.edu.pe>
2025-08-21 21:49:10 -0500

    Add files via upload

---
+++ PROP_3_JAVA/src/PROP_3_JAVA/Main.java
@@ -0,0 +1,17 @@
+package PROP_3_JAVA;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] frecuencia = new int[6];
+		
+		for(int i=0; i<20000; i++) {
+			int cara = (int)(Math.random()*6); //escoge un numero del 1 al 6
+			frecuencia[cara]++;
+		}
+		
+		for(int i=0; i<frecuencia.length; i++) {//length no lleva()
+			System.out.println("La frecuencia del numero "+(i+1)+" es: "+frecuencia[i]);
+		}
+	}
+
+}
